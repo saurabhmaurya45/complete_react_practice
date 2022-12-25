86d1c9da155a648332eceaf827090fa239efd7ae
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2022-12-26 01:46:56 +0530

    creatin seperate componentfor date ie ExpenseDate and passing data into it

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
     // const expenseDate = new Date(2021,2,24);
     // const expenseTitle = "Car Insurance";
     // const expensePrice = 256.34;
-    const month = props.date.toLocaleString("en-US", { month: "long" });
-    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-    const year = props.date.getFullYear();
+    // const month = props.date.toLocaleString("en-US", { month: "long" });
+    // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+    // const year = props.date.getFullYear();
     return (
         // <div className='expense-item'>
         //     <div>
@@ -18,12 +19,13 @@ function ExpenseItem(props){
         //     </div>
         // </div>
         <div className='expense-item'>
-            <div>
-               {/* { props.date.toISOString()} */}
+            {/* <div>
+               { props.date.toISOString()}
                 <div>{month}</div>
                 <div>{year}</div>
                 <div>{day}</div>
-            </div>
+            </div> */}
+            <ExpenseDate  date = {props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
