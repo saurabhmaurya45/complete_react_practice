52c018b2738b1e17f5625510112c95ec64ff484b
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2022-12-26 01:12:45 +0530

    learning props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,27 @@
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-    const expenseDate = new Date(2021,2,24);
-    const expenseTitle = "Car Insurance";
-    const expensePrice = 256.34;
+function ExpenseItem(props){
+    // const expenseDate = new Date(2021,2,24);
+    // const expenseTitle = "Car Insurance";
+    // const expensePrice = 256.34;
 
     return (
+        // <div className='expense-item'>
+        //     <div>
+        //         {expenseDate.toISOString()}
+        //     </div>
+        //     <div className='expense-item__description'>
+        //         <h2>{expenseTitle}</h2>
+        //         <div className='expense-item__price'>${expensePrice}</div>
+        //     </div>
+        // </div>
         <div className='expense-item'>
             <div>
-                {expenseDate.toISOString()}
+                {props.date.toISOString()}
             </div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expensePrice}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
