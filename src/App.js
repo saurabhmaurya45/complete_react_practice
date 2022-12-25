5d4bf271bfc47b7373d455c34341144038507797
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2022-12-26 02:11:01 +0530

    adding a separate  Display component to display list of expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import ExpenseItem from './components/ExpenseItem'
+import DisplayExpenses from './components/DisplayExpenses';
+import './components/Expenses.css';
 
 function App() {
   const expenses = [
@@ -31,10 +32,10 @@ function App() {
   return (
     <>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <div className='expenses'>
+          <DisplayExpenses expenses = {expenses}/>
+      </div>
+      
     </>
   );
 }
