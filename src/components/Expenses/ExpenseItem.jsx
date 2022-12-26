996fc97f8c5ac7b32199a29d3aabd875f5ff3e7e
saurabhmaurya45 <tgtsaurabhmaurya45@gmail.com>
2022-12-27 01:20:53 +0530

    learning about state

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,37 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Cards from '../UI/Cards';
 
 function ExpenseItem(props){
-    // const expenseDate = new Date(2021,2,24);
-    // const expenseTitle = "Car Insurance";
-    // const expensePrice = 256.34;
-    // const month = props.date.toLocaleString("en-US", { month: "long" });
-    // const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-    // const year = props.date.getFullYear();
+    const [title, setTitle] = useState(props.title);
+    
+    
+    const clickHandler = ()=>{
+        setTitle("updated");
+    }
     return (
-        // <div className='expense-item'>
-        //     <div>
-        //         {expenseDate.toISOString()}
-        //     </div>
-        //     <div className='expense-item__description'>
-        //         <h2>{expenseTitle}</h2>
-        //         <div className='expense-item__price'>${expensePrice}</div>
-        //     </div>
-        // </div>
         <Cards className='expense-item'>
-            {/* <div>
-               { props.date.toISOString()}
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div> */}
             <ExpenseDate  date = {props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick = {clickHandler}> Click</button>
         </Cards>
     );
     
